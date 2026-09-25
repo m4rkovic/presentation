822148f474317ae02bd5ef30de7a46ca3693d015
m4rkovic <46758911+m4rkovic@users.noreply.github.com>
2026-09-25 13:37:53 +0200

    Fix motion article closing tag

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -203,7 +203,7 @@ export default function HomePage() {
                   </p>
                 </div>
               </div>
-            </article>
+            </motion.article>
           ))}
         </section>
 
